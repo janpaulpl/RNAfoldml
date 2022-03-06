bac6fa080be8707628e7c304ec3a0fab5bf89bdc
jpVinnie <janpaul.ramos@protonmail.com>
2022-03-06 01:55:10 -0500

    add `names` attribute back

--- test/main.ml
+++ test/main.ml
@@ -9,20 +9,28 @@ let [ t1 ] = rna_from_fasta "test_data/test1.fasta"
 let [ t2; t3; t4 ] = rna_from_fasta "test_data/test2.fasta"
 let broken_fasta = rna_from_fasta "test_data/broken.fasta"
 
-let get_seq_test (input : rna) (expected_output : string) : test =
-  "" >:: fun _ -> assert_equal expected_output (get_seq input)
+let get_seq_test
+    (name : string)
+    (input : rna)
+    (expected_output : string) : test =
+  name >:: fun _ -> assert_equal expected_output (get_seq input)
 
 let get_info_test
+    (name : string)
     (input : rna)
     (expected_output : (string * string) list) : test =
-  "" >:: fun _ -> assert_equal expected_output (get_info input)
+  name >:: fun _ -> assert_equal expected_output (get_info input)
 
-let get_name_test (input : rna) (expected_output : string) : test =
-  "" >:: fun _ -> assert_equal expected_output (get_name input)
+let get_name_test
+    (name : string)
+    (input : rna)
+    (expected_output : string) : test =
+  name >:: fun _ -> assert_equal expected_output (get_name input)
 
 let rna_tests =
   [
-    get_seq_test t1 "AAAGCGGUUUGUUCCACUCGCGUUCAGUUAGCGUUUGCAGUUCUGGGCUC";
+    get_seq_test "Single sequence FASTA" t1
+      "AAAGCGGUUUGUUCCACUCGCGUUCAGUUAGCGUUUGCAGUUCUGGGCUC";
   ]
 
 let tests = "test suite" >::: List.flatten [ rna_tests ]
