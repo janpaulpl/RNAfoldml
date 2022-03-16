25f2116bf49474dc2332e2573895ea6bc1208fef
Inle Bush <imb33@cornell.edu>
2022-03-15 20:28:02 -0400

    Remove Regex

--- src/lib/rna.ml
+++ src/lib/rna.ml
@@ -16,35 +16,40 @@ let rep_ok r =
     raise Invalid_RI
   else r
 
-(** [string_from_fasta] is the parsed data from the fasta file as a string. Gets rid of 
-    whitespace and [;] comments. *)
-let string_from_fasta filename = let ch = open_in filename in 
-  let string_fasta = really_input_string ch (in_channel_length ch) in 
-  close_in ch; 
-  (fun r -> Str.global_replace (Str.regexp ";[^\n]*\n") "" string_fasta) "" 
-  |> Str.global_replace (Str.regexp " ") ""
+(** [string_from_fasta] is the parsed data from the fasta file as a
+    string. *)
+let string_from_fasta filename =
+  let ch = open_in filename in
+  let string_fasta = really_input_string ch (in_channel_length ch) in
+  close_in ch;
+  string_fasta
 
-(** [get_from_fasta] searches for a particular regex in a given sequence string*)
-let get_from_fasta s rgx = 
-  Str.global_replace (Str.regexp rgx) "\\1" s
+(** [rna_from_single_fasta s] is the rna of type [t] represented by the
+    single entry fasta style string [s].
 
-(** [rna_from_single_fasta] builds a [t] from a single sequence (string). *)
-let rna_from_single_fasta s = 
-  { 
-    seq =  get_from_fasta s "\\([AGCU]*\\)[^\n]*\n*"; 
-    name = get_from_fasta s ">[^-]*-" ; (* This returns literally the opposite *)
-    info =  get_from_fasta s "\\(-[^\n]*\n\\)"; (* This too *)
-  }
-  
-(** [rna_from_fasta] is the [t list] with [i] sequence: 
-    name, information, and sequence data). *)
+    ex) name\nAAAUUUAUAUUA ex) >name info1 info2 info3\nAAUAUAUAU
+    AUAUAUAUU\n AUAUAUAUAU AUAUAUAAUAUA\n*)
+let rna_from_single_fasta s =
+  let name_len =
+    match String.index_opt s ' ' with
+    | None -> Invalid_argument "Fasta file without name" |> raise
+    | Some i -> i
+  in
+  { seq = s; name = String.sub s 0 name_len; info = "" }
+
+(** [rna_from_fasta] is the [t list] with [i] sequence: name,
+    information, and sequence data). *)
 let rna_from_fasta f =
   let s = string_from_fasta f in
-  let sequences = String.split_on_char '>' s in
+  let sequences =
+    match String.split_on_char '>' s with
+    | [] -> []
+    | h :: t -> if String.length h > 0 && h.[0] = ';' then t else h :: t
+  in
   let x : t list = List.map rna_from_single_fasta sequences in
-    x
-    |> List.map (fun r -> try [rep_ok r] with Invalid_RI -> [])  
-    |> List.flatten
+  x
+  |> List.map (fun r -> try [ rep_ok r ] with Invalid_RI -> [])
+  |> List.flatten
 
 let rna_from_string s name =
   try rep_ok { seq = s; name; info = "" }
@@ -54,4 +59,3 @@ let rna_from_string s name =
 let get_seq r = r.seq
 let get_info r = r.info
 let get_name r = r.name
-
